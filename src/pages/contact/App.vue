7f47ccdd6623f12e3b26c20236ca74794e05110b
Corentin <clashcocoytb@gmail.com>
2025-12-14 17:22:27 +0100

    Added contact page + refacto

---
+++ src/pages/contact/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import langOption from '@/components/LangOption.vue'
+    import NavBar from '@/components/NavBar.vue'
+    import Sections from '@/components/Sections.vue'
+</script>
+
+<template>
+    <body>
+        <div class="main">
+        <NavBar/>
+        <div class="content">
+            <Sections :sections="$tm('contact.sections')"/>
+        </div>
+        <langOption/>
+        </div>
+    </body>
+</template>
+
+<style lang="css">
+    @import '@/assets/styles/pages/about.css';
+</style>
--- src/pages/ongoing-projects/App.vue
+++ src/pages/ongoing-projects/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import langOption from '@/components/LangOption.vue'
-import NavBar from '@/components/NavBar.vue'
-import Sections from '@/components/Sections.vue'
+    import langOption from '@/components/LangOption.vue'
+    import NavBar from '@/components/NavBar.vue'
+    import Sections from '@/components/Sections.vue'
+    import Contact from '@/components/Contact.vue'
 </script>
 
 <template>
   <body>
     <div class="main">
       <NavBar/>
+      <Contact/>
       <div class="content">
         <Sections :sections="$tm('ongoing-projects.sections')"/>
       </div>
