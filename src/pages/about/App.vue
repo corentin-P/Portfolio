93e6596745e47031cd75d6745076d54f691c1d5e
piquerez <corentin.piquerez1@etu.univ-lorraine.fr>
2025-12-20 18:37:07 +0100

    modified buttons in about

--- src/pages/about/App.vue
+++ src/pages/about/App.vue
@@ -16,12 +16,12 @@
                 <img :src="$t('about.sumup.profile_image')" :alt="$t('about.sumup.profile_image_desc')">
                 <div class="text">
                     <p v-for="text in $tm('about.sumup.text')">{{ text }}</p> 
-                    <a class="button button-arrow button-right" :href="`${ $t('about.sumup.projects.url') }`">{{ $t("about.sumup.projects.text") }}</a>
+                    <a class="button button-arrow button-right" :href="`${ $t('about.sumup.get-resume.url') }`">{{ $t("about.sumup.get-resume.text") }}</a>
                 </div>
             </div>
             <Sections :sections="$tm('about.resume.sections')"/>
             <div class="center space">
-                <a class="button button-arrow" :href="`${ $t('about.resume.get-resume.file') }`" download>{{ $t("about.resume.get-resume.text") }}</a>
+                <a class="button button-arrow" :href="`${ $t('about.resume.projects.url') }`">{{ $t("about.resume.projects.text") }}</a>
             </div>
             <!---->
         </div>
