7506d8bb7885408af8d15fbb374a3063ba66ac67
Corentin <clashcocoytb@gmail.com>
2026-05-12 22:49:28 +0200

    fix: gallery styles

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
   import { ref } from 'vue'
+  import Tags from '@/components/Tags.vue'
+  
   defineProps(['modalContent', 'isModalOpen'])
   const emit = defineEmits(['update:isModalOpen'])
 
@@ -16,7 +18,7 @@
       <hr>
       <h1>
         {{ modalContent.title }}
-        <a :href="modalContent.main_link" target="_blank" class="main-link">
+        <a :href="modalContent.main_link" target="_blank" class="main-link" v-if="modalContent.main_link">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/>
           </svg>
@@ -24,17 +26,7 @@
       </h1>
       <hr>
       <p v-for="line in modalContent.description">{{ line }}</p>
-      <div class="tags">
-        <span class="tag" v-for="tag in modalContent.tags">
-          <div v-if="tag.includes('::')">
-            <span class="tag-left">{{ tag.split('::')[0] }}</span>
-            <span class="tag-right">{{ tag.split('::')[1] }}</span>
-          </div>
-          <div v-else>
-            {{ tag }}
-          </div>
-        </span>
-      </div>
+      <Tags :tags="modalContent.tags"/>
       <div class="icon-links">
         <a v-for="link in modalContent.links" :href="link.link" target="_blank">
           <img :src="link.icon" alt="link.alt" class="icon"/>
---
+++ src/components/Tags.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    defineProps(['tags'])
+</script>
+
+<template>
+    <div class="tags">
+        <span class="tag" v-for="tag in tags">
+            <div v-if="tag.includes('::')">
+                <span class="tag-left">{{ tag.split('::')[0] }}</span>
+                <span class="tag-right">{{ tag.split('::')[1] }}</span>
+            </div>
+            <template v-else>
+                {{ tag }}
+            </template>
+        </span>
+    </div>
+</template>
+
+<style>
+    @import '@/assets/styles/components/tags.css';
+</style>
